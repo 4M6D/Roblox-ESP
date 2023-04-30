cd6a4fd47f8184de806402af0cdf63b4a15fd823
4M6D <132174657+4M6D@users.noreply.github.com>
2023-04-30 14:11:45 -0500

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,10 +1,11 @@
+if _G.LoadedESP == false then
 assert(Drawing, "missing dependency: drawing");
 
 --Script made by mickey and UncleRon(4M6D)
 
 --//Checking to see if there is already settings
-if TeamCheck == false then
-    else if TeamCheck == true then
+if _G.TeamCheck == false then
+    else if _G.TeamCheck == true then
         else
         _G.TeamCheck = false
         _G.BoxEspI = true
@@ -142,7 +143,8 @@ local function vischeckthing(espc, vis, on)
 local function update_esp()
     for player, esp in next, cache do
         local character = player and player.Character;
-        if character and player.Team ~= localplayer.Team or _G.TeamCheck == false then
+        if _G.TeamCheck == true then
+        if character and player.Team ~= localplayer.Team then
             local cframe = get_pivot(character);
             local position, visible = wtvp(camera, cframe.Position);
             
@@ -286,6 +288,152 @@ local function update_esp()
             esp.lua.Visible = false;
             esp.lut.Visible = false;
         end
+        else
+            if character then
+            local cframe = get_pivot(character);
+            local position, visible = wtvp(camera, cframe.Position);
+            
+            --Mind the fucking terrible skeleton esp this is my first time making it
+            
+            --//Torso and head shit
+            local lowerTorso = get_pivot(character.LowerTorso);
+            local ltpos, vislt = wtvp(camera, lowerTorso.Position);
+            local upperTorso = get_pivot(character.UpperTorso);
+            local utpos, visut = wtvp(camera, upperTorso.Position);
+            local Chead = get_pivot(character.Head);
+            local headpos, vishead = wtvp(camera, Chead.Position);
+            
+            --//Right leg shit
+            local rightFoot = get_pivot(character.RightFoot);
+            local rfpos, visrf = wtvp(camera, (rightFoot.Position));
+            local rightLowerLeg = get_pivot(character.RightLowerLeg);
+            local rllpos, visrll = wtvp(camera, rightLowerLeg.Position);
+            local rightUpperLeg = get_pivot(character.RightUpperLeg);
+            local rulpos, visrul = wtvp(camera, rightUpperLeg.Position);
+            
+            --//Left leg shit
+            local leftFoot = get_pivot(character.LeftFoot);
+            local lfpos, vislf = wtvp(camera, leftFoot.Position);
+            local leftLowerLeg = get_pivot(character.LeftLowerLeg);
+            local lllpos, vislll = wtvp(camera, leftLowerLeg.Position);
+            local leftUpperLeg = get_pivot(character.LeftUpperLeg);
+            local lulpos, vislul = wtvp(camera, leftUpperLeg.Position);
+            
+            --// Right arm shit
+            local rightHand = get_pivot(character.RightHand)
+            local rhpos, visrh = wtvp(camera, rightHand.Position)
+            local rightLowerArm = get_pivot(character.RightLowerArm)
+            local rlapos, visrla = wtvp(camera, rightLowerArm.Position)
+            local rightUpperArm = get_pivot(character.RightUpperArm)
+            local ruapos, visrua = wtvp(camera, rightUpperArm.Position)
+            
+            --// Left arm shit
+            local leftHand = get_pivot(character.LeftHand)
+            local lhpos, vislh = wtvp(camera, leftHand.Position)
+            local leftLowerArm = get_pivot(character.LeftLowerArm)
+            local llapos, vislla = wtvp(camera, leftLowerArm.Position)
+            local leftUpperArm = get_pivot(character.LeftUpperArm)
+            local luapos, vislua = wtvp(camera, leftUpperArm.Position)
+            
+            
+            vischeckthing(esp.box, visible, _G.BoxEspI)
+            vischeckthing(esp.tracer, visible, _G.TracerEspI)
+            vischeckthing(esp.name, visible, _G.NameEspI)
+            vischeckthing(esp.distance, visible, _G.DistanceEspI)
+            vischeckthing(esp.rlleg, visible, _G.SkeletonEspI)
+            vischeckthing(esp.ruleg, visible, _G.SkeletonEspI)
+            vischeckthing(esp.rltorso, visible, _G.SkeletonEspI)
+            vischeckthing(esp.llleg, visible, _G.SkeletonEspI)
+            vischeckthing(esp.luleg, visible, _G.SkeletonEspI)
+            vischeckthing(esp.lltorso, visible, _G.SkeletonEspI)
+            vischeckthing(esp.utorso, visible, _G.SkeletonEspI)
+            vischeckthing(esp.head, visible, _G.SkeletonEspI)
+            vischeckthing(esp.rh, visible, _G.SkeletonEspI)
+            vischeckthing(esp.rua, visible, _G.SkeletonEspI)
+            vischeckthing(esp.rla, visible, _G.SkeletonEspI)
+            vischeckthing(esp.rut, visible, _G.SkeletonEspI)
+            vischeckthing(esp.lh, visible, _G.SkeletonEspI)
+            vischeckthing(esp.lla, visible, _G.SkeletonEspI)
+            vischeckthing(esp.lua, visible, _G.SkeletonEspI)
+            vischeckthing(esp.lut, visible, _G.SkeletonEspI)
+
+            if visible then
+                local scale_factor = 1 / (position.Z * tan(rad(camera.FieldOfView * 0.5)) * 2) * 100;
+                local width, height = floor(35 * scale_factor), floor(50 * scale_factor);
+                local x, y = floor(position.X), floor(position.Y);
+                
+                esp.box.Size = new_vector2(width, height);
+                esp.box.Position = new_vector2(floor(x - width * 0.5), floor(y - height * 0.5));
+
+                esp.rh.From = new_vector2(floor(rhpos.X), floor(rhpos.Y))
+                esp.rh.To = new_vector2(floor(rlapos.X), floor(rlapos.Y))
+                esp.rla.From = new_vector2(floor(rlapos.X), floor(rlapos.Y))
+                esp.rla.To = new_vector2(floor(ruapos.X), floor(ruapos.Y))
+                esp.rua.From = new_vector2(floor(rlapos.X), floor(rlapos.Y))
+                esp.rua.To = new_vector2(floor(ruapos.X), floor(ruapos.Y))
+                esp.rut.From = new_vector2(floor(ruapos.X), floor(ruapos.Y))
+                esp.rut.To = new_vector2(floor(utpos.X), floor(utpos.Y))
+                
+                esp.lh.From = new_vector2(floor(lhpos.X), floor(lhpos.Y))
+                esp.lh.To = new_vector2(floor(llapos.X), floor(llapos.Y))
+                esp.lla.From = new_vector2(floor(llapos.X), floor(llapos.Y))
+                esp.lla.To = new_vector2(floor(luapos.X), floor(luapos.Y))
+                esp.lua.From = new_vector2(floor(llapos.X), floor(llapos.Y))
+                esp.lua.To = new_vector2(floor(luapos.X), floor(luapos.Y))
+                esp.lut.From = new_vector2(floor(luapos.X), floor(luapos.Y))
+                esp.lut.To = new_vector2(floor(utpos.X), floor(utpos.Y))
+
+                esp.rlleg.From = new_vector2(floor(rfpos.X), floor(rfpos.Y));
+                esp.rlleg.To = new_vector2(floor(rllpos.X), floor(rllpos.Y));
+                esp.rlleg.From = new_vector2(floor(rllpos.X), floor(rllpos.Y));
+                esp.rlleg.To = new_vector2(floor(rulpos.X), floor(rulpos.Y));
+                esp.rltorso.From = new_vector2(floor(rulpos.X), floor(rulpos.Y))
+                esp.rltorso.To = new_vector2(floor(ltpos.X), floor(ltpos.Y))
+                
+                esp.llleg.From = new_vector2(floor(lfpos.X), floor(lfpos.Y));
+                esp.llleg.To = new_vector2(floor(lllpos.X), floor(lllpos.Y));
+                esp.llleg.From = new_vector2(floor(lllpos.X), floor(lllpos.Y));
+                esp.llleg.To = new_vector2(floor(lulpos.X), floor(lulpos.Y));
+                esp.lltorso.From = new_vector2(floor(lulpos.X), floor(lulpos.Y))
+                esp.lltorso.To = new_vector2(floor(ltpos.X), floor(ltpos.Y))
+                
+                esp.utorso.From = new_vector2(floor(ltpos.X), floor(ltpos.Y))
+                esp.utorso.To = new_vector2(floor(utpos.X), floor(utpos.Y))
+                esp.head.From = new_vector2(floor(utpos.X), floor(utpos.Y))
+                esp.head.To = new_vector2(floor(headpos.X), floor(headpos.Y))
+                
+                esp.tracer.From = new_vector2(floor(viewport_size.X * 0.5), floor(viewport_size.Y));
+                esp.tracer.To = new_vector2(x, floor(y + height * 0.5));
+
+                esp.name.Text = player.Name;
+                esp.name.Position = new_vector2(x, floor(y - height * 0.5 - esp.name.TextBounds.Y));
+
+                esp.distance.Text = floor(position.Z) .. " studs";
+                esp.distance.Position = new_vector2(x, floor(y + height * 0.5));
+            end
+        else
+            esp.box.Visible = false;
+            esp.tracer.Visible = false;
+            esp.name.Visible = false;
+            esp.distance.Visible = false;
+            esp.rlleg.Visible = false;
+            esp.ruleg.Visible = false;
+            esp.rltorso.Visible = false;
+            esp.llleg.Visible = false;
+            esp.luleg.Visible = false;
+            esp.lltorso.Visible = false;
+            esp.utorso.Visible = false;
+            esp.head.Visible = false;
+            esp.rh.Visible = false;
+            esp.rla.Visible = false;
+            esp.rua.Visible = false;
+            esp.rut.Visible = false;
+            esp.lh.Visible = false;
+            esp.lla.Visible = false;
+            esp.lua.Visible = false;
+            esp.lut.Visible = false;
+        end
+        end
     end
 end
 
@@ -298,4 +446,6 @@ for _, player in next, players:GetPlayers() do
     if player ~= localplayer then
         create_esp(player);
     end
+end
+_G.LoadedESP = true
 end
